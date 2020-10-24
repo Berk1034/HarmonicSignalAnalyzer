1a5c60aebb2597f71cc016d311d4ae32768bee24
Constantin Hitov <kostya.khitov@mail.ru>
2020-10-24 19:09:22 +0300

    Complete task

---
+++ HarmonicSignalAnalyzer/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace HarmonicSignalAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_Graphics = new System.Windows.Forms.PictureBox();
+            this.lbl_FiAngle = new System.Windows.Forms.Label();
+            this.tb_FiAngle = new System.Windows.Forms.TextBox();
+            this.btn_Calculate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Graphics)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb_Graphics
+            // 
+            this.pb_Graphics.BackColor = System.Drawing.SystemColors.Window;
+            this.pb_Graphics.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb_Graphics.Location = new System.Drawing.Point(0, 0);
+            this.pb_Graphics.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pb_Graphics.Name = "pb_Graphics";
+            this.pb_Graphics.Size = new System.Drawing.Size(862, 453);
+            this.pb_Graphics.TabIndex = 0;
+            this.pb_Graphics.TabStop = false;
+            this.pb_Graphics.Resize += new System.EventHandler(this.pb_Graphics_Resize);
+            // 
+            // lbl_FiAngle
+            // 
+            this.lbl_FiAngle.AutoSize = true;
+            this.lbl_FiAngle.Location = new System.Drawing.Point(272, 20);
+            this.lbl_FiAngle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_FiAngle.Name = "lbl_FiAngle";
+            this.lbl_FiAngle.Size = new System.Drawing.Size(59, 17);
+            this.lbl_FiAngle.TabIndex = 1;
+            this.lbl_FiAngle.Text = "FiAngle:";
+            // 
+            // tb_FiAngle
+            // 
+            this.tb_FiAngle.Location = new System.Drawing.Point(336, 18);
+            this.tb_FiAngle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_FiAngle.Name = "tb_FiAngle";
+            this.tb_FiAngle.Size = new System.Drawing.Size(66, 22);
+            this.tb_FiAngle.TabIndex = 2;
+            this.tb_FiAngle.Text = "22,5";
+            // 
+            // btn_Calculate
+            // 
+            this.btn_Calculate.Location = new System.Drawing.Point(439, 16);
+            this.btn_Calculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Calculate.Name = "btn_Calculate";
+            this.btn_Calculate.Size = new System.Drawing.Size(99, 32);
+            this.btn_Calculate.TabIndex = 3;
+            this.btn_Calculate.Text = "Calculate";
+            this.btn_Calculate.UseVisualStyleBackColor = true;
+            this.btn_Calculate.Click += new System.EventHandler(this.btn_Calculate_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_FiAngle);
+            this.panel1.Controls.Add(this.btn_Calculate);
+            this.panel1.Controls.Add(this.tb_FiAngle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 389);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(862, 64);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 453);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pb_Graphics);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Graphics)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_Graphics;
+        private System.Windows.Forms.Label lbl_FiAngle;
+        private System.Windows.Forms.TextBox tb_FiAngle;
+        private System.Windows.Forms.Button btn_Calculate;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ HarmonicSignalAnalyzer/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HarmonicSignalAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        const int Xpadding = 40;
+        const int XStep = 20;
+        const float YStep = 0.1f;
+        int YMiddle;
+        const int N = 256;
+        const int K = 1;//3 * N / 4;
+        const float deltaBasis = 0.707f;
+        const int maxAmplitude = 1;
+
+        Bitmap bm;
+        readonly Font font = new Font("Arial", 9);
+        readonly Brush brush = new SolidBrush(Color.Black);
+        const int xRange = 5 * N;
+        const int yRange = 2 * maxAmplitude;
+
+        float PixelsInX => (pb_Graphics.Width - Xpadding) / (float)xRange;
+        float PixelsInY => pb_Graphics.Height / (float)yRange;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawDeltaGraphics(PointF[] points, Color color)
+        {
+            points = (PointF[])points.Clone();
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].X = Xpadding + (points[i].X - K) * PixelsInX;
+                points[i].Y = YMiddle - points[i].Y * PixelsInY;
+            }
+
+            var graphics = Graphics.FromImage(bm);
+            DrawDeltaAxis(graphics);
+
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                graphics.DrawLine(new Pen(color, 2), points[i], points[i + 1]);
+            }
+
+            pb_Graphics.Image = bm;
+        }
+
+        private void DrawDeltaAxis(Graphics graphics)
+        {
+            graphics.DrawLine(Pens.Black, Xpadding, YMiddle, pb_Graphics.Width, YMiddle);
+            graphics.DrawLine(Pens.Black, Xpadding, 0, Xpadding, pb_Graphics.Height);
+
+            for (int x = K; x < 5 * N; x += XStep)
+            {
+                var pixel = (x - K) * PixelsInX + Xpadding;
+                graphics.DrawLine(Pens.Black, pixel, YMiddle - 5, pixel, YMiddle + 5);
+                graphics.DrawString($"{ x }", font, brush, pixel - 4, YMiddle + 7);
+            }
+
+            for (double y = maxAmplitude; y >= -maxAmplitude; y -= YStep)
+            {
+                graphics.DrawLine(Pens.Black, Xpadding + 5, YMiddle + (int)(y * PixelsInY), Xpadding - 5, YMiddle + (int)(y * PixelsInY));
+                graphics.DrawString($"{-1 * y:0.0}", font, brush, Xpadding - 35, YMiddle + (int)(y * PixelsInY) - 8);
+            }
+        }
+
+        private void CalculateCharacteristics(double fi0)
+        {
+            List<float> ms = new List<float>();
+            int k = K;
+            while (k <= 5 * N)
+            {
+                ms.Add(k);
+                k += 1;
+            }
+
+            var Xi = new double[5 * N];
+            for (int i = 0; i < Xi.Length; i++)
+            {
+                Xi[i] = Math.Sin(2 * Math.PI * i / N + fi0);
+            }
+
+            var rootMeanSquares1 = new float[ms.Count()];
+            var rootMeanSquares2 = new float[ms.Count()];
+            var amplitudes = new float[ms.Count()];
+            var rootMeanSquareDeltas1 = new PointF[ms.Count()];
+            var rootMeanSquareDeltas2 = new PointF[ms.Count()];
+            var amplitudeDeltas = new PointF[ms.Count()];
+
+            for (int i = 0; i < ms.Count; i++)
+            {
+                double sumX2 = 0;
+                double sumX = 0;
+                double reSum = 0;
+                double imSum = 0;
+                for (int j = 0; j < ms[i]; j++)
+                {
+                    sumX2 += Math.Pow(Xi[j], 2);
+                    sumX += Xi[j];
+                    reSum += Xi[j] * Math.Cos(2 * Math.PI * j  / ms[i]);
+                    imSum += Xi[j] * Math.Sin(2 * Math.PI * j  / ms[i]);
+                }
+
+                rootMeanSquares1[i] = (float)Math.Sqrt(sumX2 / (ms[i] + 1));
+                rootMeanSquares2[i] = (float)Math.Sqrt(sumX2 / (ms[i] + 1) - Math.Pow(sumX / (ms[i] + 1), 2));
+                amplitudes[i] = (float)Math.Sqrt(Math.Pow(2 / ms[i] * reSum, 2) + Math.Pow(2 / ms[i] * imSum, 2));
+
+                rootMeanSquareDeltas1[i] = new PointF(ms[i], deltaBasis - rootMeanSquares1[i]);
+                rootMeanSquareDeltas2[i] = new PointF(ms[i], deltaBasis - rootMeanSquares2[i]);
+                amplitudeDeltas[i] = new PointF(ms[i], maxAmplitude - amplitudes[i]);
+            }
+
+            bm = new Bitmap(pb_Graphics.Width, pb_Graphics.Height);
+            DrawDeltaGraphics(rootMeanSquareDeltas1, Color.Aqua);
+            DrawDeltaGraphics(rootMeanSquareDeltas2, Color.Green);
+            DrawDeltaGraphics(amplitudeDeltas, Color.Blue);
+        }
+
+        private void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            double FiAngle = double.Parse(tb_FiAngle.Text, CultureInfo.InvariantCulture);
+            FiAngle = FiAngle * Math.PI / 180;
+            CalculateCharacteristics(FiAngle);
+        }
+
+        private void pb_Graphics_Resize(object sender, EventArgs e)
+        {
+            YMiddle = pb_Graphics.Height / 2;
+        }
+    }
+}
